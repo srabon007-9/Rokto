9527bd8e603e44edac6dd4ecf349c4fdd9a17aab
srabon4u <srabonahmed2002@gmail.com>
2024-11-29 05:34:31 +0600

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Donors from './components/Donors/Donors'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 
@@ -12,6 +13,7 @@ function App() {
       <Navbar></Navbar>
       <Header></Header>
       <Donors></Donors>
+      <Footer></Footer>
     </>
   )
 }
